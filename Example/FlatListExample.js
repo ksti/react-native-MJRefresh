6e74de7e936ff81f8dc9a8d12eb926d3e777e257
GJS <1353990812@qq.com>
2019-05-14 11:00:03 +0800

    js: Example 中添加测试代码：this._flatList._listRef._scrollRef.scrollTo({x: 0, y: 0})

--- Example/FlatListExample.js
+++ Example/FlatListExample.js
@@ -8,7 +8,10 @@ export default class FlatListExample extends Component {
     constructor(props){
         super(props);
         this.state = {
-            data: ['row 1', 'row 2','row 3','row 4','row 5','row 6','row 7','row 8'],
+            data: [
+                'row 1', 'row 2','row 3','row 4','row 5','row 6','row 7','row 8','row 9','row 10',
+                'row 11', 'row 12','row 13','row 14','row 15','row 16','row 17','row 18','row 19',
+            ],
         };
     }
     _onRefresh=()=>{
@@ -16,14 +19,19 @@ export default class FlatListExample extends Component {
             this._hw && this._hw.finishRefresh()
         },1000)
     }
+    onItemPress = () => {
+        alert(111)
+        this._flatList && this._flatList._listRef._scrollRef && this._flatList._listRef._scrollRef.scrollTo({x: 0, y: 0})
+    }
     render() {
         return (
             <View style={{flex: 1}}>
                 <FlatList
                     //legacyImplementation 如果需要使用此属性，应从mjrefresh插件中引入Flatlist
                     keyExtractor={(item)=>item}
+                    ref={ref=>this._flatList = ref}
                     data={this.state.data}
-                    renderItem={({item,index}) => <Text key={index} onPress={()=>alert(111)} style={{height:100}}>{item}</Text>}
+                    renderItem={({item,index}) => <Text key={index} onPress={this.onItemPress} style={{height:100}}>{item}</Text>}
                     renderScrollComponent={props=><ScrollView
                         style={{flex:1}}
                         refreshControl={
